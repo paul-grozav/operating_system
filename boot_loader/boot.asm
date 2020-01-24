d4d0497b567fa1efeeb3cb58680674920061554c
Tancredi-Paul Grozav <paul@grozav.info>
2020-01-24 15:40:59 +0200

    graphics mode initial test.

--- boot_loader/boot.asm
+++ boot_loader/boot.asm
@@ -1,28 +1,58 @@
 # Author: Tancredi-Paul Grozav <paul@grozav.info>
 # ============================================================================ #
-.code16 # tell the assembler that we're using 16 bit mode
+.code64 # tell the assembler that we're using 16 bit mode
 .global init # makes our label "init" available to the outside
 
 init: # this is the beginning of our binary later.
-  # Note The dollar sign is used in front of constant numeric values.
+# mov $0x0e, %ah
+# int $0x10 # call the function in ah from interrupt 0x10
 
-  # Set AH to value 0xe which is the teletype function. This function prints a
-  # character and automatically advances the cursor. The character printer by
-  # this function will be read from(must be set to) AL.
   mov $0x0e, %ah
+  mov $0x57, %al # 57=W
+  int $0x10
 
-  # Set the character we want to be printed: X (or 0x58 in hex)
-  mov $0x58, %al
+  mov $0x00, %ah			# subfunction 0
+  mov $0x04, %al			# select mode 18 (or 12h if prefer)
+  int $0x10				# call graphics interrupt
 
-  # Call the function in AH (teletype) from interrupt 0x10(video)
-  int $0x10 # call the function in ah from interrupt 0x10
+  mov $0x0e, %ah
+  mov $0x58, %al # 58=X
+  int $0x10
 
-  # Print another character by setting AL and calling the interrupt
-  mov $0x59, %al
+  # Set color 1=blue
+  mov $0x01, %al
+  mov $0x0c, %ah
+  # Set pixel X(cx), set Y(dx) and draw (int 10)
+  mov $0x01, %cx
+  mov $0x01, %dx
+  int $0x10
+  # Set pixel X(cx), set Y(dx) and draw (int 10)
+  mov $0x02, %cx
+  mov $0x01, %dx
+  int $0x10
+  # Set pixel X(cx), set Y(dx) and draw (int 10)
+  mov $0x03, %cx
+  mov $0x01, %dx
+  int $0x10
+
+  mov $0x00, %ah
+  # wait for keypress
+  int $0x16
+
+  mov $0x00, %ah			# again subfunc 0
+  mov $0x03, %al			# text mode 3
+  int $0x10				# call int
+  mov $0x04c, %ah
+  mov $0x00, %al			# end program normally
+  int $0x21
+
+  mov $0x0e, %ah
+  mov $0x59, %al # 59=Y
   int $0x10
 
   hlt # stops executing. Note! avoid: jmp init
-  # If we would call init again, that would print same character forever
+# ===========================================================
+# ===========================================================
 
 # The last parameter is the value that fills the binary file, up to 510 bytes.
 # It can be any value, not just 0. And you can see it using:
