f14a41e41b719a8f44931f6c0a7f3051d08f4f04
Tancredi-Paul Grozav <paul@grozav.info>
2020-09-04 21:14:23 +0300

    ore

--- project/src/module__network.c
+++ project/src/module__network.c
@@ -9,103 +9,51 @@
 #include "module_heap.h"
 #include "module_interrupt.h"
 // -------------------------------------------------------------------------- //
-// -----
-typedef uint32_t be32;
-typedef uint16_t be16;
-
-#define __const_htons(x) ((((x) & 0xFF00) >> 8) | (((x) & 0x00FF) << 8));
-#define __const_ntohs __const_htons
-// -------------------------------------------------------------------------- //
-static inline uint16_t ntohs(uint16_t x) {
-        return (((x & 0xFF00) >> 8) | ((x & 0x00FF) << 8));
-}
-
-static inline uint16_t htons(uint16_t x) {
-        return (((x & 0xFF00) >> 8) | ((x & 0x00FF) << 8));
-}
-
-static inline uint32_t ntohl(uint32_t x) {
-        return (
-                ((x & 0xFF000000) >> 24) |
-                ((x & 0x00FF0000) >> 8)  |
-                ((x & 0x0000FF00) << 8)  |
-                ((x & 0x000000FF) << 24)
-        );
-}
-
-static inline uint32_t htonl(uint32_t x) {
-        return (
-                ((x & 0xFF000000) >> 24) |
-                ((x & 0x00FF0000) >> 8)  |
-                ((x & 0x0000FF00) << 8)  |
-                ((x & 0x000000FF) << 24)
-        );
-}
-// -------------------------------------------------------------------------- //
 //static const module__network__mac_address broadcast_mac =
 //  {{0xff, 0xff, 0xff, 0xff, 0xff, 0xff}};
 //static const module__network__mac_address zero_mac = {{0, 0, 0, 0, 0, 0}};
 // -------------------------------------------------------------------------- //
-void module__network__print_mac(const module__network__mac_address * const ma)
+// Packet Utils functions
+// -------------------------------------------------------------------------- //
+static inline uintptr_t round_down(uintptr_t val, uintptr_t place)
 {
-  char buffer[20+1];
-  size_t l = 0;
-  for (uint8_t i=0; i<6; i++)
-  {
-    l = module_base_uint64_to_ascii_base16((uint8_t)(ma->data[i]), buffer);
-    if(l == 1)
-    {
-      buffer[2] = '\0';
-      buffer[1] = buffer[0];
-      buffer[0] = '0';
-    } else {
-      buffer[l] = '\0';
-    }
-    module_terminal_global_print_c_string(buffer);
-    if(i<5)
-    {
-      module_terminal_global_print_c_string(":");
-    }
-  }
+  return val & ~(place - 1);
 }
 // -------------------------------------------------------------------------- //
-void print_ip(const uint32_t ip)
+static inline uintptr_t round_up(uintptr_t val, uintptr_t place)
 {
-  for (uint8_t i=0; i<4; i++)
-  {
-    module_terminal_global_print_uint64( ((const uint8_t * const)(&ip)) [i] );
-    if(i < 3)
-    {
-      module_terminal_global_print_c_string(".");
-    }
-  }
+  return round_down(val + place - 1, place);
 }
 // -------------------------------------------------------------------------- //
-struct __attribute__((__packed__)) ethernet_header
+static inline uint16_t ntohs(uint16_t x)
 {
-  module__network__mac_address destination_mac;
-  module__network__mac_address source_mac;
-  be16 ethertype;
-  char data[];
-};
+  return (((x & 0xFF00) >> 8) | ((x & 0x00FF) << 8));
+}
 // -------------------------------------------------------------------------- //
-enum arp_op {
-    ARP_REQ = 1,
-    ARP_RESP = 2,
-};
+static inline uint16_t htons(uint16_t x)
+{
+  return (((x & 0xFF00) >> 8) | ((x & 0x00FF) << 8));
+}
 // -------------------------------------------------------------------------- //
-struct __attribute__((__packed__)) arp_header {
-    // eth_hdr
-    be16 hw_type;
-    be16 proto;
-    uint8_t hw_size;
-    uint8_t proto_size;
-    be16 op;
-    module__network__mac_address sender_mac;
-    be32 sender_ip;
-    module__network__mac_address target_mac;
-    be32 target_ip;
-};
+static inline uint32_t ntohl(uint32_t x)
+{
+  return (
+    ((x & 0xFF000000) >> 24) |
+    ((x & 0x00FF0000) >> 8) |
+    ((x & 0x0000FF00) << 8) |
+    ((x & 0x000000FF) << 24)
+  );
+}
+// -------------------------------------------------------------------------- //
+static inline uint32_t htonl(uint32_t x)
+{
+  return (
+    ((x & 0xFF000000) >> 24) |
+    ((x & 0x00FF0000) >> 8) |
+    ((x & 0x0000FF00) << 8) |
+    ((x & 0x000000FF) << 24)
+  );
+}
 // -------------------------------------------------------------------------- //
 void print_hex_bytes(const uint8_t * const base, const size_t count)
 {
@@ -131,53 +79,50 @@ void print_hex_bytes(const uint8_t * const base, const size_t count)
   module_terminal_global_print_c_string("\n");
 }
 // -------------------------------------------------------------------------- //
-static inline uintptr_t round_down(uintptr_t val, uintptr_t place) {
-  return val & ~(place - 1);
-}
-static inline uintptr_t round_up(uintptr_t val, uintptr_t place) {
-  return round_down(val + place - 1, place);
-}
-// -------------------------------------------------------------------------- //
-const struct ethernet_header * eth_hdr(const module__network__packet * const pk)
+void module__network__print_mac(const module__network__mac_address * const ma)
 {
-  return (const struct ethernet_header * const)&(pk->buffer);
+  char buffer[20+1];
+  size_t l = 0;
+  for (uint8_t i=0; i<6; i++)
+  {
+    l = module_base_uint64_to_ascii_base16((uint8_t)(ma->data[i]), buffer);
+    if(l == 1)
+    {
+      buffer[2] = '\0';
+      buffer[1] = buffer[0];
+      buffer[0] = '0';
+    } else {
+      buffer[l] = '\0';
+    }
+    module_terminal_global_print_c_string(buffer);
+    if(i<5)
+    {
+      module_terminal_global_print_c_string(":");
+    }
+  }
 }
 // -------------------------------------------------------------------------- //
-void print_eth_hdr(const struct ethernet_header * const h)
+void module__network__print_ip(const uint32_t ip)
 {
-  module_terminal_global_print_c_string("ethernet_header");
-  module_terminal_global_print_c_string("{ \"source_mac\": \"");
-  module__network__print_mac(&(h->source_mac));
-  module_terminal_global_print_c_string("\", \"destination_mac\": \"");
-  module__network__print_mac(&(h->destination_mac));
-  module_terminal_global_print_c_string("\", \"type\": \"");
-  const uint16_t eth_type = ntohs(h->ethertype);
-  if(eth_type == module__network__ethernet_packet_type__arp)
+  for (uint8_t i=0; i<4; i++)
   {
-    module_terminal_global_print_c_string("ARP");
+    module_terminal_global_print_uint64( ((const uint8_t * const)(&ip)) [i] );
+    if(i < 3)
+    {
+      module_terminal_global_print_c_string(".");
+    }
   }
-  else if(eth_type == module__network__ethernet_packet_type__ip)
-  {
-    module_terminal_global_print_c_string("IP");
-  }
-  else
-  {
-    module_terminal_global_print_c_string("UNKNOWN");
-  }
-  module_terminal_global_print_c_string("(");
-  module_terminal_global_print_uint64(eth_type);
-  module_terminal_global_print_c_string(")");
-  module_terminal_global_print_c_string("\" }");
-  module_terminal_global_print_c_string("\n");
 }
 // -------------------------------------------------------------------------- //
-void print_arp_header(const struct arp_header * const h)
+// ARP Packet
+// -------------------------------------------------------------------------- //
+void print_arp_header(const module__network__arp_header * const h)
 {
   module_terminal_global_print_c_string("arp_header");
   module_terminal_global_print_c_string("{ \"sender_ip\": \"");
-  print_ip(h->sender_ip);
+  module__network__print_ip(h->sender_ip);
   module_terminal_global_print_c_string("\", \"target_ip\": \"");
-  print_ip(h->target_ip);
+  module__network__print_ip(h->target_ip);
   module_terminal_global_print_c_string("\", \"nothing\": \"");
 /*
   module__network__print_mac(&(h->destination_mac));
@@ -199,17 +144,43 @@ void print_arp_header(const struct arp_header * const h)
   module_terminal_global_print_c_string("\n");
 }
 // -------------------------------------------------------------------------- //
-const struct arp_header * arp_hdr(const module__network__packet * const pk)
-{
-  return (const struct arp_header * const)(
-    pk->buffer + sizeof(struct ethernet_header)
-  );
-}
-// -------------------------------------------------------------------------- //
 void process_arp_packet(const module__network__packet * const p)
 {
-  const struct arp_header * const arp = arp_hdr(p);
+  const module__network__arp_header * const arp =
+    (const module__network__arp_header * const)(
+    p->buffer + sizeof(module__network__ethernet_header)
+  );
   print_arp_header(arp);
+}
+// -------------------------------------------------------------------------- //
+// Ethernet Packet
+// -------------------------------------------------------------------------- //
+void print_eth_hdr(const module__network__ethernet_header * const h)
+{
+  module_terminal_global_print_c_string("ethernet_header");
+  module_terminal_global_print_c_string("{ \"source_mac\": \"");
+  module__network__print_mac(&(h->source_mac));
+  module_terminal_global_print_c_string("\", \"destination_mac\": \"");
+  module__network__print_mac(&(h->destination_mac));
+  module_terminal_global_print_c_string("\", \"type\": \"");
+  const uint16_t eth_type = ntohs(h->ethertype);
+  if(eth_type == module__network__ethernet_header_type__arp)
+  {
+    module_terminal_global_print_c_string("ARP");
+  }
+  else if(eth_type == module__network__ethernet_header_type__ip)
+  {
+    module_terminal_global_print_c_string("IP");
+  }
+  else
+  {
+    module_terminal_global_print_c_string("UNKNOWN");
+  }
+  module_terminal_global_print_c_string("(");
+  module_terminal_global_print_uint64(eth_type);
+  module_terminal_global_print_c_string(")");
+  module_terminal_global_print_c_string("\" }");
+  module_terminal_global_print_c_string("\n");
 }
 // -------------------------------------------------------------------------- //
 void module__network__process_ethernet_packet(
@@ -217,14 +188,15 @@ void module__network__process_ethernet_packet(
 {
   print_hex_bytes(p->buffer, p->length);//rx_buff_size);
 //  print_hex_bytes(rx_buffer, 64);//rx_buff_size);
-  const struct ethernet_header * const eth = eth_hdr(p);
+  const module__network__ethernet_header * const eth =
+    (const module__network__ethernet_header * const)&(p->buffer);
   print_eth_hdr(eth);
   const uint16_t eth_type = ntohs(eth->ethertype);
-  if(eth_type == module__network__ethernet_packet_type__arp)
+  if(eth_type == module__network__ethernet_header_type__arp)
   {
     process_arp_packet(p);
   }
-  else if(eth_type == module__network__ethernet_packet_type__ip)
+  else if(eth_type == module__network__ethernet_header_type__ip)
   {
 //    module_terminal_global_print_c_string("Got IP packet.\n");
   }
--- project/src/module__network.h
+++ project/src/module__network.h
@@ -20,14 +20,15 @@ typedef struct
 
   /**
    * Sequence of bytes following this structure, in the allocated heap zone.
+   * These bytes for an ethernet_packet
    */
   uint8_t buffer[]; // why not equivalent to "uint8_t * buffer" ?
 } module__network__packet;
 // -------------------------------------------------------------------------- //
-enum module__network__ethernet_packet_type
+enum module__network__ethernet_header_type
 {
-  module__network__ethernet_packet_type__ip = 0x0800,
-  module__network__ethernet_packet_type__arp = 0x0806,
+  module__network__ethernet_header_type__ip = 0x0800,
+  module__network__ethernet_header_type__arp = 0x0806,
 };
 // -------------------------------------------------------------------------- //
 typedef struct __attribute__((__packed__))
@@ -35,9 +36,37 @@ typedef struct __attribute__((__packed__))
   uint8_t data[6];
 } module__network__mac_address;
 // -------------------------------------------------------------------------- //
+typedef struct __attribute__((__packed__))
+{
+  module__network__mac_address destination_mac;
+  module__network__mac_address source_mac;
+  uint16_t ethertype;
+  uint8_t buffer[];
+} module__network__ethernet_header;
+// -------------------------------------------------------------------------- //
+typedef struct __attribute__((__packed__))
+{
+  // eth_hdr
+  uint16_t hw_type;
+  uint16_t proto;
+  uint8_t hw_size;
+  uint8_t proto_size;
+  uint16_t op;
+  module__network__mac_address sender_mac;
+  uint32_t sender_ip;
+  module__network__mac_address target_mac;
+  uint32_t target_ip;
+} module__network__arp_header;
+// -------------------------------------------------------------------------- //
+//enum arp_op {
+//    ARP_REQ = 1,
+//    ARP_RESP = 2,
+//};
+// -------------------------------------------------------------------------- //
 //! Run short network test
 void module__network__test();
 void module__network__print_mac(const module__network__mac_address * const ma);
+void module__network__print_ip(const uint32_t ip);
 void module__network__process_ethernet_packet(
   const module__network__packet * const p);
 // -------------------------------------------------------------------------- //
