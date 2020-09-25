09b964f00084d7461601d3053dc4d8db3b4c481f
Tancredi-Paul Grozav <paul@grozav.info>
2020-09-25 09:16:27 +0300

    compile automation

--- run.sh
+++ run.sh
@@ -24,11 +24,14 @@ if [ ! -f ${prefix}/bin/${target}-ld ]; then
   version="2.33.1" &&
   mkdir -p ${work_path}/binutils &&
   cd ${work_path}/binutils &&
-  ([ ! -f binutils-${version}.tar.gz ] && curl https://ftp.gnu.org/gnu/binutils/binutils-${version}.tar.gz --output binutils-${version}.tar.gz || true) &&
+  ([ ! -f binutils-${version}.tar.gz ] && curl \
+    https://ftp.gnu.org/gnu/binutils/binutils-${version}.tar.gz --output \
+    binutils-${version}.tar.gz || true) &&
   ([ ! -d binutils-${version} ] && tar xf binutils-${version}.tar.gz || true) &&
   mkdir -p build-binutils &&
   cd build-binutils &&
-  ../binutils-${version}/configure --target=${target} --prefix="${prefix}" --with-sysroot --disable-nls --disable-werror &&
+  ../binutils-${version}/configure --target=${target} --prefix="${prefix}" \
+    --with-sysroot --disable-nls --disable-werror &&
   make -j $(nproc) &&
   make install
 fi &&
@@ -38,11 +41,14 @@ if [ ! -f ${prefix}/bin/${target}-gcc ]; then
   version="9.2.0" &&
   mkdir -p ${work_path}/gcc &&
   cd ${work_path}/gcc &&
-  ([ ! -f gcc-${version}.tar.gz ] && curl https://ftp.gnu.org/gnu/gcc/gcc-${version}/gcc-${version}.tar.gz --output gcc-${version}.tar.gz || true) &&
+  ([ ! -f gcc-${version}.tar.gz ] && curl \
+    https://ftp.gnu.org/gnu/gcc/gcc-${version}/gcc-${version}.tar.gz --output \
+    gcc-${version}.tar.gz || true) &&
   ([ ! -d gcc-${version} ] && tar xf gcc-${version}.tar.gz || true) &&
   mkdir -p build-gcc &&
   cd build-gcc &&
-  ../gcc-${version}/configure --target=${target} --prefix="${prefix}" --disable-nls --enable-languages=c,c++ --without-headers &&
+  ../gcc-${version}/configure --target=${target} --prefix="${prefix}" \
+    --disable-nls --enable-languages=c,c++ --without-headers &&
   make all-gcc -j $(nproc) &&
   make all-target-libgcc -j $(nproc) &&
   make install-gcc &&
@@ -54,31 +60,27 @@ echo "Compiling kernel ..." &&
 cd ${work_path}/project &&
 rm -rf build && mkdir -p build && cd build &&
 src_folder="${work_path}/project/src" &&
+source_files="kernel" &&
+modules="kernel terminal serial base interrupt heap video video_mode video_font\
+  keyboard pci _driver__rtl8139 _network" &&
+for m in ${modules}; do
+  source_files="${source_files} module_${m}"
+done &&
 # Compile
-c_compiler_params="${target}-gcc -std=gnu99 -ffreestanding -g3 -O1 -Wall -Wextra -I${src_folder}" &&
-${c_compiler_params} -c ${src_folder}/start.s -o start.o &&
-${c_compiler_params} -c ${src_folder}/kernel.c -o kernel.o &&
-${c_compiler_params} -c ${src_folder}/module_kernel.c -o module_kernel.o &&
-${c_compiler_params} -c ${src_folder}/module_terminal.c -o module_terminal.o &&
-${c_compiler_params} -c ${src_folder}/module_serial.c -o module_serial.o &&
-${c_compiler_params} -c ${src_folder}/module_base.c -o module_base.o &&
-${c_compiler_params} -c ${src_folder}/module_interrupt.c -o module_interrupt.o &&
-${c_compiler_params} -c ${src_folder}/module_interrupt_asm.s -o module_interrupt_asm.o &&
-${c_compiler_params} -c ${src_folder}/module_heap.c -o module_heap.o &&
-${c_compiler_params} -c ${src_folder}/module_video.c -o module_video.o &&
-${c_compiler_params} -c ${src_folder}/module_video_mode.c -o module_video_mode.o &&
-${c_compiler_params} -c ${src_folder}/module_video_font.c -o module_video_font.o &&
-${c_compiler_params} -c ${src_folder}/module_keyboard.c -o module_keyboard.o &&
-${c_compiler_params} -c ${src_folder}/module_pci.c -o module_pci.o &&
-${c_compiler_params} -c ${src_folder}/module__driver__rtl8139.c -o module__driver__rtl8139.o &&
-${c_compiler_params} -c ${src_folder}/module__network.c -o module__network.o &&
+c_compiler_params="${target}-gcc -std=gnu99 -ffreestanding -g3 -O1 -Wall \
+  -Wextra -I${src_folder}" &&
+${target}-as ${src_folder}/start.s -o start.o &&
+${target}-as ${src_folder}/module_interrupt_asm.s -o module_interrupt_asm.o &&
+for f in ${source_files}; do
+  ${c_compiler_params} -c ${src_folder}/${f}.c -o ${f}.o
+done
 # Link
-${target}-gcc -ffreestanding -nostdlib -g -T ../linker.ld \
-  start.o kernel.o module_kernel.o module_terminal.o module_serial.o \
-  module_base.o module_interrupt.o module_interrupt_asm.o module_heap.o \
-  module_video.o module_video_mode.o module_video_font.o module_keyboard.o \
-  module_pci.o module__network.o module__driver__rtl8139.o \
-  -lgcc -o my_kernel.elf &&
+object_list="start.o module_interrupt_asm.o" &&
+for f in ${source_files}; do
+  object_list="${object_list} ${f}.o"
+done
+${target}-gcc -ffreestanding -nostdlib -g -T ../linker.ld ${object_list} -lgcc \
+  -o my_kernel.elf &&
 echo "Compiled kernel!" &&
 
 # Make ISO
@@ -89,9 +91,10 @@ set timeout=0 # Wait 0 seconds for the user to choose the item, or use default
 set default=0 # Set the default menu entry - index 0 (first)
 CONFIG_PANIC_TIMEOUT=10
 
-menuentry "My Operating System" {
-   multiboot /boot/kernel-file   # The multiboot command replaces the kernel command
-   boot
+menuentry "My Operating System"
+{
+  multiboot /boot/kernel-file #The multiboot command replaces the kernel command
+  boot
 }
 EOF
 ) &&
