f974e3673ea20587204f34a3b1d9a786fb943f8a
Tancredi-Paul Grozav <paul@grozav.info>
2020-10-02 21:24:28 +0300

    add run cmd to script.

---
+++ project/run.sh
@@ -0,0 +1,18 @@
+# ============================================================================ #
+# Author: Tancredi-Paul Grozav <paul@grozav.info>
+# ============================================================================ #
+current_directory="$(cd $(dirname $0); pwd)" &&
+docker exec -it os bash /mnt/run.sh &&
+(
+qemu-system-x86_64 \
+  -m 400M \
+  -cdrom ${current_directory}/build/bootable.iso \
+  -boot d \
+  -device rtl8139,netdev=net0,mac=52:54:00:12:13:56 \
+  -netdev user,id=net0 \
+  -object filter-dump,id=f1,netdev=net0,file=${current_directory}/dump.dat \
+  &
+) &&
+# sleep 3 && ( sleep 1 | telnet 127.0.0.1 5556 )
+exit 0
+# ============================================================================ #
