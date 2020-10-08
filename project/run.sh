30843c6e585bce4e4631803288f487f0be8f146a
Tancredi-Paul Grozav <paul@grozav.info>
2020-10-09 00:11:31 +0300

    removed hardcoded mac, and added bootp options. Got valid DHCP offer.

--- project/run.sh
+++ project/run.sh
@@ -4,11 +4,12 @@
 current_directory="$(cd $(dirname $0); pwd)" &&
 docker exec -it os bash /mnt/run.sh &&
 (
+#  -device rtl8139,netdev=net0,mac=52:54:00:12:13:56 \
 qemu-system-x86_64 \
   -m 400M \
   -cdrom ${current_directory}/build/bootable.iso \
   -boot d \
-  -device rtl8139,netdev=net0,mac=52:54:00:12:13:56 \
+  -device rtl8139,netdev=net0 \
   -netdev user,id=net0 \
   -object filter-dump,id=f1,netdev=net0,file=${current_directory}/dump.dat \
   &
--- project/src/module__network__service__dhcp__client.c
+++ project/src/module__network__service__dhcp__client.c
@@ -52,7 +52,7 @@ void module__network__service__dhcp__client__get_net_config(
     bph->hardware_address_type = 1; // 1 = Ethernet
     bph->hardware_address_length = sizeof (module__network__data__mac_address);
     bph->gateway_hops = 0;
-    bph->transaction_id = module__network__data__htonl(0x89cdf841); // ??? does it matter?
+    bph->transaction_id = module__network__data__htonl(1);//0x89cdf841);//random
     bph->seconds_since_boot = 0; // just booted
     bph->flags = 0;
     bph->client_ip_address = module__network__data__ip(0, 0, 0, 0);
@@ -65,14 +65,82 @@ void module__network__service__dhcp__client__get_net_config(
       interface->mac_address;
     module_kernel_memset(bph->server_host_name, 0, 64);
     module_kernel_memset(bph->boot_file_name, 0, 128);
-    char * vd =
-                              "\x63\x82\x53\x63\x35\x01\x01\x32\x04\x0a"\
-      "\x00\x02\x0f\x0c\x06\x64\x65\x62\x69\x61\x6e\x37\x0d\x01\x1c\x02"\
-      "\x03\x0f\x06\x77\x0c\x2c\x2f\x1a\x79\x2a\x3d\x13\xff\x00\x12\x13"\
-      "\x56\x00\x01\x00\x01\x26\xd1\x38\xdb\x52\x54\x00\x12\x34\x56\xff"\
-      "\x00\x00\x00\x00\x00\x00";
-    module_kernel_memcpy(vd, bph->vendor_area, 64);
+    {
+//      char * vd =
+//                                "\x63\x82\x53\x63\x35\x01\x01\x32\x04\x0a"
+//        "\x00\x02\x0f\x0c\x06\x64\x65\x62\x69\x61\x6e\x37\x0d\x01\x1c\x02"
+//        "\x03\x0f\x06\x77\x0c\x2c\x2f\x1a\x79\x2a\x3d\x13\xff\x00\x12\x13"
+//        "\x56\x00\x01\x00\x01\x26\xd1\x38\xdb\x52\x54\x00\x12\x34\x56\xff"
+//        "\x00\x00\x00\x00\x00\x00";
+//      module_kernel_memcpy(vd, bph->vendor_area, 64);
+      module_kernel_memset(bph->vendor_area, 0, 64);
 
+      size_t i = 0;
+      // DHCP magic cookie - an identifier
+      *(uint32_t *)(void*)(bph->vendor_area + i) =
+        module__network__data__htonl(0x63825363); // important to keep it.
+      i += sizeof(uint32_t);
+
+      // Option 53 - DHCP message type
+      *(uint8_t *)(bph->vendor_area + i++) = 53;
+      *(uint8_t *)(bph->vendor_area + i++) = 1; // length = 1
+      *(uint8_t *)(bph->vendor_area + i++) = 1; // value 1 = DHCP discover
+
+      // Option 50 - requested IP address - last known ip address.
+      // is not mandatory that it will give you this address
+      *(uint8_t *)(bph->vendor_area + i++) = 50;
+      *(uint8_t *)(bph->vendor_area + i++) = 4; // length = 4
+      *(uint32_t *)(void*)(bph->vendor_area + i) =
+        module__network__data__ip(192, 168, 0, 13); // value 1 = wanted IP addr
+      i += sizeof(uint32_t);
+
+      // Option 12 - client host name
+      *(uint8_t *)(bph->vendor_area + i++) = 12;
+      *(uint8_t *)(bph->vendor_area + i++) = 6; // length = 6
+      module_kernel_memcpy("TediOS", (bph->vendor_area + i), 6);
+      i += 6;
+
+      // Option 55 - Parameter request list
+      *(uint8_t *)(bph->vendor_area + i++) = 55;
+      *(uint8_t *)(bph->vendor_area + i++) = 13; // length = 13
+      // List items
+      *(uint8_t *)(bph->vendor_area + i++) = 1; // subnet mask
+      *(uint8_t *)(bph->vendor_area + i++) = 28; // broadcast address
+      *(uint8_t *)(bph->vendor_area + i++) = 2; // time offset
+      *(uint8_t *)(bph->vendor_area + i++) = 3; // router
+      *(uint8_t *)(bph->vendor_area + i++) = 15; // domain name
+      *(uint8_t *)(bph->vendor_area + i++) = 6; // domain name server
+      *(uint8_t *)(bph->vendor_area + i++) = 119; // domain search
+      *(uint8_t *)(bph->vendor_area + i++) = 12; // host name
+      *(uint8_t *)(bph->vendor_area + i++) = 44; // NetBIOS over TCP/IP name srv
+      *(uint8_t *)(bph->vendor_area + i++) = 47; // NetBIOS over TCP/IP scope
+      *(uint8_t *)(bph->vendor_area + i++) = 26; // interface mtu
+      *(uint8_t *)(bph->vendor_area + i++) = 121; // classless static route
+      *(uint8_t *)(bph->vendor_area + i++) = 42; // NetTimeProto servers
+
+      // Option 61 - client identifier
+      *(uint8_t *)(bph->vendor_area + i++) = 61;
+      *(uint8_t *)(bph->vendor_area + i++) = 19; // length = 19
+      *(uint8_t *)(bph->vendor_area + i++) = 255; // what is this ? padding ??
+      *(uint32_t *)(void*)(bph->vendor_area + i) =
+        module__network__data__htonl(1);//0x00121356); // IAID
+      i += sizeof(uint32_t);
+      *(uint16_t *)(void*)(bph->vendor_area + i) =
+        module__network__data__htons(1); // DUID type= link layer address + time
+      i += sizeof(uint16_t);
+      *(uint16_t *)(void*)(bph->vendor_area + i) =
+        module__network__data__htons(1); // Hardware type = ethernet
+      i += sizeof(uint16_t);
+      *(uint32_t *)(void*)(bph->vendor_area + i) =
+        module__network__data__htonl(1);//0x26d138db); // Time
+      i += sizeof(uint32_t);
+      *((module__network__data__mac_address *)(bph->vendor_area + i)) =
+        interface->mac_address;
+      i += sizeof(module__network__data__mac_address);
+
+      // Option 255 - END of options list - no length & value
+      *(uint8_t *)(bph->vendor_area + i++) = 255;
+    }
 
     iph->total_length = module__network__data__htons(
       sizeof(module__network__data__ip_header)
@@ -84,31 +152,31 @@ void module__network__service__dhcp__client__get_net_config(
     module__network__data__ip__checksum(p);
 
 
-    module__network__data__packet * p2;
-    p2 = module__network__data__packet__create_with_data(
-"\xff\xff\xff\xff\xff\xff\x52\x54\x00\x12\x13\x56\x08\x00\x45\x10"
-"\x01\x48\x00\x00\x00\x00\x80\x11\x39\x96\x00\x00\x00\x00\xff\xff"
-"\xff\xff\x00\x44\x00\x43\x01\x34\xd3\x9b\x01\x01\x06\x00\x89\xcd"
-"\xf8\x41\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
-"\x00\x00\x00\x00\x00\x00\x52\x54\x00\x12\x13\x56\x00\x00\x00\x00"
-"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
-"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
-"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
-"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
-"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
-"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
-"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
-"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
-"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
-"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
-"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
-"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
-"\x00\x00\x00\x00\x00\x00\x63\x82\x53\x63\x35\x01\x01\x32\x04\x0a"
-"\x00\x02\x0f\x0c\x06\x64\x65\x62\x69\x61\x6e\x37\x0d\x01\x1c\x02"
-"\x03\x0f\x06\x77\x0c\x2c\x2f\x1a\x79\x2a\x3d\x13\xff\x00\x12\x13"
-"\x56\x00\x01\x00\x01\x26\xd1\x38\xdb\x52\x54\x00\x12\x34\x56\xff"
-"\x00\x00\x00\x00\x00\x00"
-  , 342);
+//    module__network__data__packet * p2;
+//    p2 = module__network__data__packet__create_with_data(
+//"\xff\xff\xff\xff\xff\xff\x52\x54\x00\x12\x13\x56\x08\x00\x45\x10"
+//"\x01\x48\x00\x00\x00\x00\x80\x11\x39\x96\x00\x00\x00\x00\xff\xff"
+//"\xff\xff\x00\x44\x00\x43\x01\x34\xd3\x9b\x01\x01\x06\x00\x89\xcd"
+//"\xf8\x41\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+//"\x00\x00\x00\x00\x00\x00\x52\x54\x00\x12\x13\x56\x00\x00\x00\x00"
+//"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+//"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+//"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+//"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+//"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+//"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+//"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+//"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+//"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+//"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+//"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+//"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+//"\x00\x00\x00\x00\x00\x00\x63\x82\x53\x63\x35\x01\x01\x32\x04\x0a"
+//"\x00\x02\x0f\x0c\x06\x64\x65\x62\x69\x61\x6e\x37\x0d\x01\x1c\x02"
+//"\x03\x0f\x06\x77\x0c\x2c\x2f\x1a\x79\x2a\x3d\x13\xff\x00\x12\x13"
+//"\x56\x00\x01\x00\x01\x26\xd1\x38\xdb\x52\x54\x00\x12\x34\x56\xff"
+//"\x00\x00\x00\x00\x00\x00"
+//  , 342);
 
     module_terminal_print_buffer_hex_bytes(p->buffer, 342);//(size_t)(p->length));
 
